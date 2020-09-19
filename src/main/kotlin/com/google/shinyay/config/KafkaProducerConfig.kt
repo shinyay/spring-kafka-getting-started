145c4cbbfef9753cdc86f1bf97f71d5a185a34ec
shinyay <shinya.com@gmail.com>
2020-09-19 23:26:59 +0900

    Add Configuration for Kafka producer

--- src/main/kotlin/com/google/shinyay/config/KafkaProducerConfig.kt
+++ src/main/kotlin/com/google/shinyay/config/KafkaProducerConfig.kt
@@ -1,7 +1,30 @@
 package com.google.shinyay.config
 
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.serialization.StringSerializer
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.core.ProducerFactory
 
 @Configuration
 class KafkaProducerConfig {
+
+    @Value("\${io.reflectoring.kafka.gbootstrap-servers}")
+    private val bootstrapServers: String? = null
+
+    @Bean
+    fun producerConfigs(): Map<String, Any?>? {
+        val props: MutableMap<String, Any?> = HashMap()
+        props[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = bootstrapServers
+        props[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
+        return props
+    }
+
+    @Bean
+    fun producerFactory(): ProducerFactory<String?, String?>? {
+        return DefaultKafkaProducerFactory(producerConfigs()!!)
+    }
+
 }
