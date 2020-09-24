105d46d0fdf1dc036433b4d621a4588820020d9c
shinyay <shinya.com@gmail.com>
2020-09-24 23:14:47 +0900

    Add configurations for message consumer

--- src/main/kotlin/com/google/shinyay/config/KafkaConsumerConfig.kt
+++ src/main/kotlin/com/google/shinyay/config/KafkaConsumerConfig.kt
@@ -1,6 +1,9 @@
 package com.google.shinyay.config
 
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.common.serialization.StringDeserializer
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
@@ -8,4 +11,12 @@ class KafkaConsumerConfig {
 
     @Value("\${io.reflectoring.kafka.bootstrap-servers}")
     private val bootstrapServers: String? = null
+
+    @Bean
+    fun consumerConfigs(): Map<String, Any>? {
+        val props: MutableMap<String, Any> = HashMap()
+        props[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = bootstrapServers!!
+        props[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
+        return props
+    }
 }
