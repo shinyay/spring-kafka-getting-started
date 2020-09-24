4f35272c853057fb8d31399e582b47c0e4dda8ae
shinyay <shinya.com@gmail.com>
2020-09-24 23:12:41 +0900

    Add parameter from application properties

--- src/main/kotlin/com/google/shinyay/config/KafkaConsumerConfig.kt
+++ src/main/kotlin/com/google/shinyay/config/KafkaConsumerConfig.kt
@@ -1,7 +1,11 @@
 package com.google.shinyay.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class KafkaConsumerConfig {
+
+    @Value("\${io.reflectoring.kafka.bootstrap-servers}")
+    private val bootstrapServers: String? = null
 }
