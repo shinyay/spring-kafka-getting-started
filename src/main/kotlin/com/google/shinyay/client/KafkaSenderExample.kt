148032d4a67d5113bed79de6b34aaf81a0618669
shinyay <shinya.com@gmail.com>
2020-09-26 20:47:55 +0900

    Add KafkaTemplate to send messages

--- src/main/kotlin/com/google/shinyay/client/KafkaSenderExample.kt
+++ src/main/kotlin/com/google/shinyay/client/KafkaSenderExample.kt
@@ -1,4 +1,11 @@
 package com.google.shinyay.client
 
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.stereotype.Component
+
+
+@Component
 class KafkaSenderExample {
+
+    val kafkaTemplate: KafkaTemplate<String, String>? = null
 }
