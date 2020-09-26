e161f8b29e3c6bde65cbf9e0cfbe94c93cf313fb
shinyay <shinya.com@gmail.com>
2020-09-26 20:51:51 +0900

    Add constructor for autowire KafkaTemplate

--- src/main/kotlin/com/google/shinyay/client/KafkaSenderExample.kt
+++ src/main/kotlin/com/google/shinyay/client/KafkaSenderExample.kt
@@ -1,11 +1,13 @@
 package com.google.shinyay.client
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.core.RoutingKafkaTemplate
 import org.springframework.stereotype.Component
 
 
 @Component
-class KafkaSenderExample {
+class KafkaSenderExample(kafkaTemplate: KafkaTemplate<String?, String?>?) {
 
     val kafkaTemplate: KafkaTemplate<String, String>? = null
 }
