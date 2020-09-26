57f31a18314bf18b773443f7cdf630428b763305
shinyay <shinya.com@gmail.com>
2020-09-26 20:42:34 +0900

    Add NewTopic

--- src/main/kotlin/com/google/shinyay/config/KafkaTopicConfig.kt
+++ src/main/kotlin/com/google/shinyay/config/KafkaTopicConfig.kt
@@ -1,4 +1,18 @@
 package com.google.shinyay.config
 
+import org.apache.kafka.clients.admin.NewTopic
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.config.TopicBuilder
+
+
+@Configuration
 class KafkaTopicConfig {
+
+    @Value("\${io.reflectoring.kafka.topic-1}")
+    private val topic1: String? = null
+
+    @Bean
+    fun topic1(): NewTopic? = TopicBuilder.name(topic1).build()
 }
