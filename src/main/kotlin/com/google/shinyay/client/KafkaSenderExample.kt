12f2e845f7e88088df0c1480e26ecf5a0fb883b8
shinyay <shinya.com@gmail.com>
2020-09-26 20:55:47 +0900

    Add send messages function

--- src/main/kotlin/com/google/shinyay/client/KafkaSenderExample.kt
+++ src/main/kotlin/com/google/shinyay/client/KafkaSenderExample.kt
@@ -1,13 +1,13 @@
 package com.google.shinyay.client
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.kafka.core.KafkaTemplate
-import org.springframework.kafka.core.RoutingKafkaTemplate
 import org.springframework.stereotype.Component
 
 
 @Component
-class KafkaSenderExample(kafkaTemplate: KafkaTemplate<String?, String?>?) {
+class KafkaSenderExample(val kafkaTemplate: KafkaTemplate<String?, String?>?) {
 
-    val kafkaTemplate: KafkaTemplate<String, String>? = null
+    fun sendMessage(topicName: String, message: String) {
+        kafkaTemplate?.send(topicName, message)
+    }
 }
