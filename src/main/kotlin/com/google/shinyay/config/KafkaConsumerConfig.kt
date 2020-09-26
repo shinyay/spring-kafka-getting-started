4aea860bf7e8d3d99411f08207aea3653fcf0033
shinyay <shinya.com@gmail.com>
2020-09-26 20:29:02 +0900

    Add message listener container

--- src/main/kotlin/com/google/shinyay/config/KafkaConsumerConfig.kt
+++ src/main/kotlin/com/google/shinyay/config/KafkaConsumerConfig.kt
@@ -5,8 +5,11 @@ import org.apache.kafka.common.serialization.StringDeserializer
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
+import org.springframework.kafka.config.KafkaListenerContainerFactory
 import org.springframework.kafka.core.ConsumerFactory
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer
 
 
 @Configuration
@@ -26,5 +29,12 @@ class KafkaConsumerConfig {
     @Bean
     fun consumerFactory(): ConsumerFactory<String?, String?>? {
         return DefaultKafkaConsumerFactory<String?, String?>(consumerConfigs()!!)
+    }
+
+    @Bean
+    fun kafkaListenerContainerFactory(): KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String?, String?>>? {
+        val factory = ConcurrentKafkaListenerContainerFactory<String, String>()
+        factory.consumerFactory = consumerFactory()
+        return factory
     }
 }
