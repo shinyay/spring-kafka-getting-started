257a8c75aa04be881324f04c9261ff97e85d795a
shinyay <shinya.com@gmail.com>
2020-09-27 22:19:24 +0900

    Add configuration for async callback

--- src/main/kotlin/com/google/shinyay/client/KafkaSenderExample.kt
+++ src/main/kotlin/com/google/shinyay/client/KafkaSenderExample.kt
@@ -1,7 +1,10 @@
 package com.google.shinyay.client
 
 import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.support.SendResult
 import org.springframework.stereotype.Component
+import org.springframework.util.concurrent.ListenableFuture
+import org.springframework.util.concurrent.ListenableFutureCallback
 
 
 @Component
@@ -9,5 +12,16 @@ class KafkaSenderExample(val kafkaTemplate: KafkaTemplate<String?, String?>?) {
 
     fun sendMessage(topicName: String, message: String) {
         kafkaTemplate?.send(topicName, message)
+    }
+
+    fun sendMessageWithCallback(topicName: String, message: String) {
+        val future: ListenableFuture<SendResult<String?, String?>> = kafkaTemplate!!.send(topicName, message)
+        future.addCallback(object : ListenableFutureCallback<SendResult<String?, String?>?> {
+            override fun onSuccess(result: SendResult<String?, String?>?) {
+            }
+
+            override fun onFailure(ex: Throwable) {
+            }
+        })
     }
 }
